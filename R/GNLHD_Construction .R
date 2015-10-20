d12788def98749298369b7d7a9f69a3354f7ec32
DavidDJChen <chendaijun13@mails.ucas.ac.cn>
2015-10-20 09:32:52 +0800

    Update GNLHD_Construction .R

--- R/GNLHD_Construction .R
+++ R/GNLHD_Construction .R
@@ -542,7 +542,7 @@ GNLHD<-R6Class("GNLHD",
                            if((positioni>s[j-1])&&(positioni<=s[j])){
                              positionlayeri<-j
                            }
-                           if(positioni<s[1]){
+                           if(positioni<=s[1]){
                              positionlayeri<-1
                            }
                          }
